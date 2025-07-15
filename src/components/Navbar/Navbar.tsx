db6ee8fc7c262658ad05fdd357397cdeef7a4a3f
William He <contact@williamhe.dev>
2025-07-15 01:01:37 -0700

    feat: add navmenu on mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,11 @@
 
 import { Button } from '@/components';
 import clsx from 'clsx';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import type { FC } from 'react';
+import { type FC, useEffect, useState } from 'react';
 
 export interface NavbarLinkItem {
   href: string;
@@ -18,87 +18,196 @@ export interface NavbarProps {
 }
 
 export const Navbar: FC<NavbarProps> = ({ links = [] }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const pathname = usePathname();
 
+  const toggleIsMenuOpen = () => setIsMenuOpen((prev) => !prev);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isMenuOpen]);
+
   return (
-    <nav
-      className={clsx([
-        'sticky',
-        'top-0',
-        'left-0',
-        'w-full',
-        'bg-primary-3',
-        'border-b-2',
-        'border-primary-1',
-        'text-text-light',
-        'z-50',
-      ])}
-    >
-      <div
+    <>
+      <nav
         className={clsx([
-          'grid',
-          'grid-cols-3',
-          'px-5',
-          'py-3',
-          'gap-5',
-          'items-center',
+          'sticky',
+          'top-0',
+          'left-0',
+          'w-full',
+          'bg-primary-3',
+          'border-b-2',
+          'border-primary-1',
+          'text-text-light',
+          'z-50',
         ])}
       >
-        <Link href="/" className="w-fit">
-          <Image
-            src="/images/aimu_logo_dark_bg.png"
-            alt="Aimu logo"
-            height={80}
-            width={89}
-            loading="eager"
-          />
-        </Link>
-
-        <Link href="/" className={clsx(['justify-self-center', 'w-fit'])}>
-          <Image
-            src="/images/aimu_wordmark_dark_bg.png"
-            alt="Aimu logo"
-            height={80}
-            width={131}
-            loading="eager"
-          />
-        </Link>
-
-        <button className={clsx(['w-fit', 'md:hidden', 'justify-self-end'])}>
-          <Menu size={30} className={clsx(['text-text-light'])} />
-        </button>
-
-        <Link
-          href="/book"
-          className={clsx(['hidden', 'md:flex', 'justify-self-end'])}
+        <div
+          className={clsx([
+            'grid',
+            'grid-cols-3',
+            'px-5',
+            'py-3',
+            'gap-5',
+            'items-center',
+          ])}
         >
-          <Button variant="secondary">BOOK NOW</Button>
-        </Link>
+          <Link href="/" className="w-fit">
+            <Image
+              src="/images/aimu_logo_dark_bg.png"
+              alt="Aimu logo"
+              height={80}
+              width={89}
+              loading="eager"
+            />
+          </Link>
+
+          <Link href="/" className={clsx(['justify-self-center', 'w-fit'])}>
+            <Image
+              src="/images/aimu_wordmark_dark_bg.png"
+              alt="Aimu logo"
+              height={80}
+              width={131}
+              loading="eager"
+            />
+          </Link>
+
+          <button
+            className={clsx(['w-fit', 'md:hidden', 'justify-self-end'])}
+            onClick={toggleIsMenuOpen}
+          >
+            <Menu size={30} className={clsx(['text-text-light'])} />
+          </button>
+
+          <Link
+            href="/book"
+            className={clsx(['hidden', 'md:flex', 'justify-self-end'])}
+          >
+            <Button variant="secondary">BOOK NOW</Button>
+          </Link>
+        </div>
+
+        {links.length > 0 && (
+          <ul className={clsx(['hidden', 'list-none', 'md:flex', 'px-5'])}>
+            {links.map((link, index) => {
+              const isActive = pathname === link.href;
+
+              return (
+                <Link key={`${link.href}_${index}`} href={link.href}>
+                  <li
+                    className={clsx([
+                      'px-5',
+                      'py-3',
+                      'border-primary-1',
+                      isActive && ['border-b-4', 'text-primary-1'],
+                      !isActive && ['hover:text-accent-2'],
+                    ])}
+                  >
+                    {link.label}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        )}
+      </nav>
+
+      <div
+        className={clsx([
+          'top-0',
+          'h-screen',
+          'w-screen',
+          'fixed',
+          'md:hidden',
+          'z-50',
+          { 'pointer-events-none': !isMenuOpen },
+        ])}
+      >
+        <div
+          className={clsx([
+            'size-full',
+            'backdrop-blur-lg',
+            'bg-white/50',
+            'absolute',
+            'transition',
+            isMenuOpen ? 'opacity-100' : 'opacity-0',
+            { 'pointer-events-none': !isMenuOpen },
+          ])}
+        ></div>
+
+        <aside
+          className={clsx([
+            'h-full',
+            'absolute',
+            'overflow-y-auto',
+            'transition-all',
+            'w-4/5',
+            'bg-primary-3',
+            'border-l-2',
+            'border-primary-1',
+            'flex',
+            'flex-col',
+            'py-3',
+            'px-5',
+            'text-text-light',
+            'gap-10',
+            isMenuOpen ? 'right-0' : '-right-full',
+          ])}
+        >
+          <div
+            className={clsx([
+              'w-full',
+              'flex',
+              'items-center',
+              'text-text-light',
+              'justify-between',
+            ])}
+          >
+            <Link href="/" className="w-fit">
+              <Image
+                src="/images/aimu_wordmark_dark_bg.png"
+                alt="Aimu logo"
+                height={80}
+                width={131}
+                loading="eager"
+              />
+            </Link>
+
+            <button className="size-fit" onClick={toggleIsMenuOpen}>
+              <X size={32} />
+            </button>
+          </div>
+
+          <ul
+            className={clsx([
+              'flex',
+              'flex-col',
+              'gap-5',
+              'text-4xl',
+              'text-right',
+              'w-full',
+              'pb-10',
+              'border-b',
+              'border-text-light',
+            ])}
+          >
+            {links.map((link, index) => (
+              <li key={`${link.href}_${index}`}>
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+
+          <Link href="/book" className={clsx(['w-fit', 'self-end'])}>
+            <Button variant="secondary">BOOK NOW</Button>
+          </Link>
+        </aside>
       </div>
-
-      {links.length > 0 && (
-        <ul className={clsx(['hidden', 'list-none', 'md:flex', 'px-5'])}>
-          {links.map((link, index) => {
-            const isActive = pathname === link.href;
-
-            return (
-              <Link key={`${link.href}_${index}`} href={link.href}>
-                <li
-                  className={clsx([
-                    'px-5',
-                    'py-3',
-                    'border-primary-1',
-                    isActive && ['border-b-4', 'text-primary-1'],
-                    !isActive && ['hover:text-accent-2'],
-                  ])}
-                >
-                  {link.label}
-                </li>
-              </Link>
-            );
-          })}
-        </ul>
-      )}
-    </nav>
+    </>
   );
 };
